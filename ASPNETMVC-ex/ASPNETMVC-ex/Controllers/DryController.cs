0a36df02861af98e53dab4f3540d8409b1b05cf3
John Harper <john.harper@live.com>
2020-06-29 09:26:55 -0500

    Uninstall bootstrap3, add CSS styles for buttons

---
+++ ASPNETMVC-ex/ASPNETMVC-ex/Controllers/DryController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNETMVC_ex.Controllers
+{
+    public class DryController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
